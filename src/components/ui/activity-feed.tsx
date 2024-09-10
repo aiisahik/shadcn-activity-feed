8c81e55ec3091ae892a1fc452e0e528232030202
Jie Zhou <jie.zhou@getmpla.com>
2024-09-10 14:54:40 +0200

    rebuilt using different quote

--- src/components/ui/activity-feed.tsx
+++ src/components/ui/activity-feed.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   MessageSquareX,
@@ -11,20 +11,20 @@ import {
   GitCommit,
   GitPullRequest,
   Snowflake
-} from 'lucide-react';
+} from "lucide-react";
 
 type ActivityType =
-  | 'message'
-  | 'reaction'
-  | 'commit'
-  | 'pullRequest'
-  | 'freeze'
-  | 'denied'
-  | 'certifiedMail'
-  | 'warningMail'
-  | 'warningMessage'
-  | 'questionMessage'
-  | 'comment';
+  | "message"
+  | "reaction"
+  | "commit"
+  | "pullRequest"
+  | "freeze"
+  | "denied"
+  | "certifiedMail"
+  | "warningMail"
+  | "warningMessage"
+  | "questionMessage"
+  | "comment";
 
 export interface ActivityItem {
   timestamp: string;
@@ -41,27 +41,27 @@ export interface ActivityItem {
 
 const getIconForType = (type: ActivityType) => {
   switch (type) {
-    case 'message':
+    case "message":
       return <MessageCircle color="white" className="h-5 w-5" />;
-    case 'reaction':
+    case "reaction":
       return <ThumbsUp color="white" className="h-5 w-5" />;
-    case 'commit':
+    case "commit":
       return <GitCommit color="white" className="h-5 w-5" />;
-    case 'pullRequest':
+    case "pullRequest":
       return <GitPullRequest color="white" className="h-5 w-5" />;
-    case 'comment':
+    case "comment":
       return <MessageCircle color="white" className="h-5 w-5" />;
-    case 'freeze':
+    case "freeze":
       return <Snowflake color="white" className="h-5 w-5" />;
-    case 'denied':
+    case "denied":
       return <MessageSquareX color="white" className="h-5 w-5" />;
-    case 'certifiedMail':
+    case "certifiedMail":
       return <MailCheck color="white" className="h-5 w-5" />;
-    case 'warningMail':
+    case "warningMail":
       return <MailWarning color="white" className="h-5 w-5" />;
-    case 'warningMessage':
+    case "warningMessage":
       return <MessageCircleWarning color="white" className="h-5 w-5" />;
-    case 'questionMessage':
+    case "questionMessage":
       return <MessageCircleQuestion color="white" className="h-5 w-5" />;
   }
 };
@@ -69,74 +69,74 @@ const getIconForType = (type: ActivityType) => {
 
 const demoActivityData: ActivityItem[] = [
   {
-    timestamp: '2024-05-01',
-    type: 'message',
-    user: { name: 'Anelli', initials: 'CH' },
-    description: 'sent initial notice of assignment to payor via email',
+    timestamp: "2024-05-01",
+    type: "message",
+    user: { name: "Anelli", initials: "CH" },
+    description: "sent initial notice of assignment to payor via email",
     content:
-      'Excepteur velit voluptate excepteur occaecat sint occaecat ex ipsum officia eiusmod fugiat. Labore proident eiusmod ut fugiat. Sit exercitation cillum quis dolore nulla laborum esse ea ad ipsum sunt occaecat ut.'
+      "Excepteur velit voluptate excepteur occaecat sint occaecat ex ipsum officia eiusmod fugiat. Labore proident eiusmod ut fugiat. Sit exercitation cillum quis dolore nulla laborum esse ea ad ipsum sunt occaecat ut."
   },
   {
-    timestamp: '2024-05-01',
-    type: 'certifiedMail',
-    user: { name: 'Anelli', initials: 'CH' },
+    timestamp: "2024-05-01",
+    type: "certifiedMail",
+    user: { name: "Anelli", initials: "CH" },
     description:
-      'sent initial notice of assignment to payor via certified mail',
+      "sent initial notice of assignment to payor via certified mail",
     content:
-      'Excepteur velit voluptate excepteur occaecat sint occaecat ex ipsum officia eiusmod fugiat. Labore proident eiusmod ut fugiat. Sit exercitation cillum quis dolore nulla laborum esse ea ad ipsum sunt occaecat ut.'
+      "Excepteur velit voluptate excepteur occaecat sint occaecat ex ipsum officia eiusmod fugiat. Labore proident eiusmod ut fugiat. Sit exercitation cillum quis dolore nulla laborum esse ea ad ipsum sunt occaecat ut."
   },
   {
-    timestamp: '2024-05-03',
-    type: 'reaction',
-    user: { name: 'Shopify', initials: 'JP' },
-    description: 'acknowledged receipt of the notice of assignment'
+    timestamp: "2024-05-03",
+    type: "reaction",
+    user: { name: "Shopify", initials: "JP" },
+    description: "acknowledged receipt of the notice of assignment"
   },
   {
-    timestamp: '2024-08-07',
-    type: 'warningMessage',
-    user: { name: 'Anelli', initials: 'JP' },
-    description: 'requested freeze of all borrower accounts via email to payor'
+    timestamp: "2024-08-07",
+    type: "warningMessage",
+    user: { name: "Anelli", initials: "JP" },
+    description: "requested freeze of all borrower accounts via email to payor"
   },
   {
-    timestamp: '2024-08-07',
-    type: 'warningMail',
-    user: { name: 'Anelli', initials: 'JP' },
+    timestamp: "2024-08-07",
+    type: "warningMail",
+    user: { name: "Anelli", initials: "JP" },
     description:
-      'requested freeze of all borrower accounts via certified mail to payor'
+      "requested freeze of all borrower accounts via certified mail to payor"
   },
   {
-    timestamp: '2024-08-15',
-    type: 'reaction',
-    user: { name: 'Shopify', initials: 'JP' },
-    description: 'acknowledged receipt of the request to freeze accounts'
+    timestamp: "2024-08-15",
+    type: "reaction",
+    user: { name: "Shopify", initials: "JP" },
+    description: "acknowledged receipt of the request to freeze accounts"
   },
   {
-    timestamp: '2024-08-19',
-    type: 'questionMessage',
-    user: { name: 'Anelli', initials: 'JP' },
-    description: 'requested an update on the status of the freeze',
+    timestamp: "2024-08-19",
+    type: "questionMessage",
+    user: { name: "Anelli", initials: "JP" },
+    description: "requested an update on the status of the freeze",
     content:
-      'Duis nisi pariatur excepteur aliqua ipsum eu ut voluptate ullamco sit do cillum labore voluptate ea.'
+      "Duis nisi pariatur excepteur aliqua ipsum eu ut voluptate ullamco sit do cillum labore voluptate ea."
   },
   {
-    timestamp: '2024-08-25',
-    type: 'freeze',
-    user: { name: 'Shopify', initials: 'JP' },
+    timestamp: "2024-08-25",
+    type: "freeze",
+    user: { name: "Shopify", initials: "JP" },
     description:
-      'verified that $23,045.00 was frozen as of 2024-08-21 on all borrower accounts'
+      "verified that $23,045.00 was frozen as of 2024-08-21 on all borrower accounts"
   },
   {
-    timestamp: '2024-09-02',
-    type: 'questionMessage',
-    user: { name: 'Anelli', initials: 'JP' },
-    description: 'requested an update on the status of the freeze'
+    timestamp: "2024-09-02",
+    type: "questionMessage",
+    user: { name: "Anelli", initials: "JP" },
+    description: "requested an update on the status of the freeze"
   },
   {
-    timestamp: '2024-09-08',
-    type: 'freeze',
-    user: { name: 'Shopify', initials: 'JP' },
+    timestamp: "2024-09-08",
+    type: "freeze",
+    user: { name: "Shopify", initials: "JP" },
     description:
-      'verified that $41,621.00 was frozen as of 2024-09-01 on all borrower accounts'
+      "verified that $41,621.00 was frozen as of 2024-09-01 on all borrower accounts"
   }
 ];
 
@@ -151,7 +151,7 @@ export default function ActivityFeed({
         <div key={index} className="mb-12 flex items-stretch h-full">
           <div className="flex-shrink-0 w-32 text-sm text-gray-400 font-semibold text-right pr-4">
             {index > 0 && activityData[index - 1].timestamp === item.timestamp
-              ? ''
+              ? ""
               : item.timestamp}
           </div>
           <div className="relative">
@@ -165,7 +165,7 @@ export default function ActivityFeed({
           </div>
           <div className="ml-4 flex-grow">
             <p className="text-sm">
-              <span className="font-semibold">{item.user.name}</span>{' '}
+              <span className="font-semibold">{item.user.name}</span>{" "}
               <span className="text-gray-500">{item.description}</span>
             </p>
             {item.content && (
